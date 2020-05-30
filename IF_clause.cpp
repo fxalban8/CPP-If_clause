daee8eca3fe1d8c5840f73774f3f36d2c15a1057
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 16:56:27 -0500

    Adding two if clauses to verify whether the input is the same as the password or not

--- IF_clause.cpp
+++ IF_clause.cpp
@@ -7,6 +7,14 @@ int main() {
 	string input;
 	cout<<"Enter the password: "<<flush;
 	cin>>input;
-	
+
+	if(input==password){	//if the condition is true
+		cout<<"Welcome back, user"<<endl;	//then execute this instruction
+	}
+
+	if(input!=password){	//if the condition is true
+		cout<<"Access denied"<<endl;	//then execute this instruction
+
+	}	
 	return 0;
 }

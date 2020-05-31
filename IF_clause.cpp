56345e1e034a1bfdcd601ada827feea560eaad66
fxalban08 <francisco.alban@epn.edu.ec>
2020-05-30 20:17:40 -0500

    Making the user to press ENTER before ending the program

--- IF_clause.cpp
+++ IF_clause.cpp
@@ -15,6 +15,20 @@ int main() {
 	if(input!=password){	//if the condition is true
 		cout<<"Access denied"<<endl;	//then execute this instruction
 
-	}	
+	}
+
+
+	//flushing the buffer
+        int c;
+        do{
+        c=cin.get();
+        } while (c!='\n'&&c!=EOF);
+
+
+        //waiting for the user to press ENTER
+        cin.get();
+
+
+	
 	return 0;
 }
